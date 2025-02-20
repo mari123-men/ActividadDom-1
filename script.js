12a3f243356900e6a89340cfa1d1ab48abc22e08
JeffersonEscobar2024 <jefferson.escobar2024@hilasal.superate.org.sv>
2025-02-20 11:12:09 -0600

    change

---
+++ script.js
@@ -0,0 +1,8 @@
+let text = "Bienvenido a tu Mini Manual de Prácticas de Estudio para Informática"
+document.getElementById("contenido").innerHTML = text
+
+const para = document.createElement("p");
+para.innerHTML = "Aqui te enseñaremos a como sobrebibir las clases de informatica congit";
+
+document.getElementById("contenido").appendChild(para);
+console.log('hola')
